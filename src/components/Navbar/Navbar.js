f49a5f8005ffb133f484dfabada40fb41d7ad1da
artur-59 <arturfernandes59@gmail.com>
2020-11-04 23:21:12 +0000

    made some changes on navbar file. still not sure how navbar will be done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div className='navbar-container'>
-            <ul>
-                <li><Link to="/">Veganbit</Link></li>
-                <li><Link to="/calculator">Calculator</Link></li>
-                <li><Link to="/about-us">About Us</Link></li>
-                <li><Link to="/meal-plan">Meal Plan</Link></li>
+            <ul className='link-list'>
+                <li><Link className='link' to="/" className='logo'>Veganbite</Link></li>
+                <li><Link className='link' to="/calculator">Calculator</Link></li>
+                <li><Link className='link' to="/about-us">About Us</Link></li>
+                <li><Link className='link' to="/meal-plan">Meal Plan</Link></li>
             </ul>
         </div>
     )

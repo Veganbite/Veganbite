0a566391af06353d45e61cf0ddd40b264dcd929b
artur-59 <arturfernandes59@gmail.com>
2020-11-05 16:46:14 +0000

    bootstrap installed and navbar is still on the making

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../../assets/logovegan.png';
 
 const Navbar = () => {
     return (
         <div className='navbar-container'>
             <ul className='link-list'>
-                <li><Link className='link' to="/" className='logo'>Veganbite</Link></li>
+                <li className='logo-li'><Link className='link' to="/" className='logo'><img src={logo} alt='Veganbite'  height='40px' /></Link></li>
                 <li><Link className='link' to="/calculator">Calculator</Link></li>
                 <li><Link className='link' to="/about-us">About Us</Link></li>
                 <li><Link className='link' to="/meal-plan">Meal Plan</Link></li>

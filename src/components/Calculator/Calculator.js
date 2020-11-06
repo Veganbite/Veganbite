3dc723a590fc99f8c997ce9829c963b153997f2d
sawnick <sawnick@DESKTOP-NMS566U.localdomain>
2020-11-06 11:56:40 +0000

    Functional Calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,45 +1,48 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
 
 class Calculator extends React.Component {
-    state = {
-        age: "",
-        weight: null,
-        gender: "",
-        height: null
-    };
+  state = {
+    age: "",
+    weight: null,
+    gender: "",
+    height: null
+  };
 
-    handleChange = (event) => {
-        const {name, value }
-        this.setState({ [name]: value }, () => {
-            this.caloricCalculator();
-        });
-    };
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    // console.log("name", name);
+    // console.log("value", value);
+    this.setState({ [name]: value }, () => {
+      this.caloricCalculator();
+    });
+  };
 
-    caloricCalculator = () => {
-        const {weight, height } = this.state;
-        if (weight && height) {
-            console.log(Number(weight) + Number(height));
-        }
-    };
-
-
+  caloricCalculator = () => {
+    const { weight, height } = this.state;
+    if (weight && height) {
+      console.log(Number(weight) + Number(height));
     }
-    render() {
-        return (
-            <div>
-                <Card 
-                title="Weight"
-                name="weight"
-                />
+  };
 
-                <Card
-                title= "Height"
-                name="height"
-                />
-               
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Card
+          title="Weight"
+          name="weight"
+          currentValue={this.state.weight}
+          handleChange={this.handleChange}
+        />
+        <Card
+          title="Height"
+          name="height"
+          currentValue={this.state.height}
+          handleChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
 
-export default Calculator
+export default Calculator;

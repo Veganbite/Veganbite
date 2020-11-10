70cd67bfe7b12b4cdfebf08a244396dd3548a5f0
artur-59 <arturfernandes59@gmail.com>
2020-11-10 17:11:02 +0000

    theres a small bug on the page height, probably on the base css file, will look into it later

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Jumbotron, Container, Row, Col, Card, Button } from 'react-bootstrap';
 import mission from '../../assets/mission.png';
 import premium from '../../assets/premium.png';
+import quotation from '../../assets/quotation.png';
 import './AboutUs.css';
 
 const AboutUs = () => {
@@ -10,7 +11,7 @@ const AboutUs = () => {
             <Jumbotron fluid className='bg-white'>
                 <Container>
                     <h1 className='title mb-4 pl-4'>About us</h1>
-                    <p>
+                    <p className='text-justify'>
                     Behind the Veganbite app, you’ll find a motivated and enthusiastic team. We truly believe that being Vegan doesn’t mean tasteless and boring meals. With Veganbite, we're helping people with tasty meal plans adapted to their body needs.
                     </p>
                 </Container>
@@ -19,30 +20,43 @@ const AboutUs = () => {
                 <Row>
                     <Col className='mb-3'>
                         <Card style={{ width: '18rem', border: 'none' }}>
-                            <img src={mission} className='mr-auto ml-auto' height='60px' width='60px'/>
+                            <img src={mission} className='mr-auto ml-auto mb-1' height='60px' width='60px'/>
                             <Card.Body className='card-body'>
-                                <Card.Title>Our Mission</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                                <Card.Title className='text-white'>Our Mission</Card.Title>
+                                <Card.Text className='text-justify text-white'>
+                                Veganbite mission: help as many people to enter the vegan world with tasty recipes. 
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col className='mb-3'>
                         <Card style={{ width: '18rem', border: 'none' }}>
-                            <img src={premium} className='mr-auto ml-auto' height='60px' width='60px'/>
+                            <img src={premium} className='mr-auto ml-auto mb-1' height='60px' width='60px'/>
                             <Card.Body>
-                                <Card.Title>Go Premium</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                                <Card.Title className='text-white'>Go Premium</Card.Title>
+                                <Card.Text className='text-justify text-white'>
+                                Everyone can try Veganbite app for free and get meal plans adapted to their body type needs and goals. With Veganbite Pro, you will have access to unlimited shuffles, more recipes and you can register to have easy access to your data. 
                                 </Card.Text>
                             </Card.Body>
                                 <Button variant="warning text-white">Go somewhere</Button>
                         </Card>
                     </Col>
                 </Row>
+            </Container>
+            <Container>
+                <h2 className='title mb-4 pl-4'>Vegan Testimonial</h2>
+                <hr/>
+                <img src={quotation} height='30px' width='30px'/>
+                <p className='text-justify'>
+                (Daniel’s girlfriend testimonial) ... Saying the difficulties and bad experiences she found when turning vegan and what she thinks about Veganbite App. ....
+                (Her name, 2020)
+                </p>
+                <hr/>
+            </Container>
+            <Container>
+                <p className='font-weight-bold'>
+                Please consult your doctor before changing your diet. The information provided in this app is designed to be a suggestion of meal plans based only on caloric intake. This information should not be used as medical advice of any kind.
+                </p>
             </Container>
         </div>
     )

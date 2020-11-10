1084f8abb7a6d967e11b9507d5f9d01b725fc4d1
artur-59 <arturfernandes59@gmail.com>
2020-11-10 15:56:34 +0000

    started working on the About us banner. Looking really really good.

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
 
 const AboutUs = () => {
     return (
         <div>
-            <h1>About Us</h1>
+            <Jumbotron fluid className='bg-white'>
+                <Container>
+                    <h1>About us</h1>
+                    <p>
+                    Behind the Veganbite app, you’ll find a motivated and enthusiastic team. We truly believe that being Vegan doesn’t mean tasteless and boring meals. With Veganbite, we're helping people with tasty meal plans adapted to their body needs.
+                    </p>
+                </Container>
+            </Jumbotron>
         </div>
     )
 }

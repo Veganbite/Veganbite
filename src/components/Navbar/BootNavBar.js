43dc347833a57952339669e76cbbc238304ebf97
artur-59 <arturfernandes59@gmail.com>
2020-11-10 15:43:37 +0000

    navbar ready for testing purposes

--- src/components/Navbar/BootNavBar.js
+++ src/components/Navbar/BootNavBar.js
@@ -6,21 +6,21 @@ import { Link } from 'react-router-dom';
 const BootNavBar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-sm navbar-light">
-            <a class="navbar-brand mr-0"><Link to="/"><img src={logo} alt='logo' height='50px'/></Link></a>
+            <nav class="navbar navbar-expand-sm navbar-dark">
+            <Link to="/" class="navbar-brand mr-0"><img src={logo} alt='logo' height='50px'/></Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <Link to="/calculator"><a class="nav-link text-white">Calculator</a></Link>
+                        <Link to="/calculator" class="nav-link text-white">Calculator</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="/meal-plan"><a class="nav-link text-white">Meal Plan</a></Link>
+                        <Link to="/meal-plan" class="nav-link text-white">Meal Plan</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="/about-us"><a class="nav-link text-white">About Us</a></Link>
+                        <Link to="/about-us" class="nav-link text-white">About Us</Link>
                     </li>
                 </ul>
             </div>

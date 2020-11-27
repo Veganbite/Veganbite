51cc3e86abbffdf0f2967d7b439a77fa25d35e52
artur-59 <arturfernandes59@gmail.com>
2020-11-27 11:06:42 +0000

    123

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div className='navbar-container'>
             <ul className='link-list'>
-                <li className='logo-li'><Link className='link' to="/" className='logo'><img src={logo} alt='Veganbite'  height='40px' /></Link></li>
+                <li className='logo-li'><Link className='link' to="/"><img src={logo} alt='Veganbite'  height='40px' /></Link></li>
                 <li><Link className='link' to="/calculator">Calculator</Link></li>
                 <li><Link className='link' to="/about-us">About Us</Link></li>
                 <li><Link className='link' to="/meal-plan">Meal Plan</Link></li>
